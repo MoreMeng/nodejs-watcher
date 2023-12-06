eeafe361e157db0215dba6bd052a8e01aaf626e1
MoreMeng <moremeng@dv4.biz>
2023-12-06 13:58:37 +0700

    bug

--- src/index.js
+++ src/index.js
@@ -33,25 +33,49 @@ cron.schedule('*/30 * * * *', () => {
 /**
  * Cron 2 : NHSO TOKEN
  */
-cron.schedule('*/36 * * * *', () => {
-  console.log("---------------------");
-  console.log("Running: " + process.env.C2_NAME);
+// cron.schedule('*/36 * * * *', () => {
+//   console.log("---------------------");
+//   console.log("Running: " + process.env.C2_NAME);
 
-  fetch(process.env.C2_URL)
-    .then((response) => response.json())
-    .then((data) => {
-      if (data.token.length < 30) {
-        //MoreMeng
-        let notify = new LineAPI.Notify({
-          token: process.env.C2_LINE_TOKEN
-        })
-        let msg = `💥 TOKEN มีปัญหาหรือหมดอายุ กรุณาเสียบบัตรประชาชนและ Login ตรวจสอบสิทธิ์ในโปรแรกม NHSO ให้ด้วยครับ บนเครื่อง "society25" หรือ "ict39"`
-        notify.send({
-          message: msg
-        })
-      }
-    })
-    .catch(function (err) {
-      console.log("Unable to fetch -", err);
-    });
+//   fetch(process.env.C2_URL)
+//     .then((response) => response.json())
+//     .then((data) => {
+//       if (data.token.length < 30) {
+//         //MoreMeng
+//         let notify = new LineAPI.Notify({
+//           token: process.env.C2_LINE_TOKEN
+//         })
+//         let msg = `💥 TOKEN มีปัญหาหรือหมดอายุ กรุณาเสียบบัตรประชาชนและ Login ตรวจสอบสิทธิ์ในโปรแรกม NHSO ให้ด้วยครับ บนเครื่อง "society25" หรือ "ict39"`
+//         notify.send({
+//           message: msg
+//         })
+//       }
+//     })
+//     .catch(function (err) {
+//       console.log("Unable to fetch -", err);
+//     });
+// })
+
+/**
+ * Cron 3 : ATH 25 BMS RTC Gateway
+ */
+cron.schedule('* * * * *', () => {
+  console.log("---------------------");
+  console.log("Running: " + process.env.C3_NAME);
+
+  isReachable(process.env.C3_URL).then(reachable => {
+    let d = new Date(Date.now()).toLocaleString();
+    console.log(`[${d}] ${process.env.C3_NAME}:  ✔ ${reachable}`);
+
+    if (!reachable) {
+      let notify = new LineAPI.Notify({
+        token: process.env.C3_LINE_TOKEN
+      })
+      //   let msg = `[${d}] NodeJS: 💥 ${reachable}`
+      let msg = `${process.env.C3_NAME} : 💥 ไม่ตอบสนอง`
+      notify.send({
+        message: msg
+      })
+    }
+  })
 })
